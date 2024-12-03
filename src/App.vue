9ae18539d4e87750cb7e1c365540f54f2db5e6f6
angelesgonzalez <delosangelesgonzalez@gmail.com>
2024-12-03 10:35:10 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-8xl font-black text-stone-700">{{ title }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "ClearMoon App",
+  data() {
+    return {
+      title: "ClearMoon App 🌕",
+    };
+  },
+};
+</script>
+<style></style>
