f7fe900ad0e7418de632169e32482bfb6a668d2c
angelesgonzalez <delosangelesgonzalez@gmail.com>
2024-12-13 13:04:32 +0100

    Created a new route and view to host the recipe details

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 
 import Home from "./views/Home.vue";
+import Recipe from "src/views/Recipe.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/recipe/:id",
+    name: "Recipe",
+    component: Recipe,
   },
 ];
 
